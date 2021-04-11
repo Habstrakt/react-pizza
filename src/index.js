f9ba7846b37f18f1c01c1d3172765dccbb8e526c
kukuruza <iamhab@gmail.com>
2021-04-11 15:20:03 +1000

    Разделение контента на главную и корзину, переход по корзине и по логотипу

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 import './app.css';
 
 ReactDOM.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <Route path='/' component={App} />
-      </BrowserRouter>
+      <Router>
+          <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
