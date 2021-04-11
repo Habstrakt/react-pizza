ed799d8ef69d6ffcfd0e90200bdda61de26efb6b
kukuruza <iamhab@gmail.com>
2021-04-11 22:34:22 +1000

    Прокидываю объект с пиццами и использую axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Route} from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import axios from "axios";
 
-const App = (props) => {
+const App = () => {
+
+		const [pizzas, setPizzas] = useState([]);
+
+		useEffect(() => {
+				axios.get('http://localhost:3000/db.json').then(({data}) => {
+						setPizzas(data.pizzas);
+				});
+		}, []);
 
 		return (
 				<div className="wrapper">
 						<Header/>
 						<div className="content">
-								<Route path="/" component={Home} exact/>
+								<Route path="/" render={() => <Home items={pizzas}  />} exact/>
 								<Route path='/cart' component={Cart} />
 						</div>
 				</div>
